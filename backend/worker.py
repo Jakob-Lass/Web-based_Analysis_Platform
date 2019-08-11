8ccd76934e3bb685a771526ba860031f8373e894
Jakob Lass <jakob@lass.dk>
2019-08-11 14:27:25 +0200

    Correct spelling mistakes

--- backend/worker.py
+++ backend/worker.py
@@ -2,10 +2,10 @@
 
 # Simple script to use redis and celery to quarry jobs to and from Flask app
 
-from celery import celery
+from celery import Celery
 from algorithm import approx
 
-app = Celery(__name__, backend='rcp://', broker='redis://')
+app = Celery(__name__, backend='rpc://', broker='redis://')
 # backend: how to extract results
 # broker: Queueing mechanism
 
